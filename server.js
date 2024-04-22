ae09bfd3a8e296c50dca03a2aecefdf0ca3c0b2a
Anjani-22 <anjaniofficial22@gmail.com>
2024-04-22 23:45:15 +0530

    Models, mogodb, query, stats aggregate, prepost (doc,query,aggre)middleware

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.NODEJSCONNECTIONSTRING.replace(
+  '<PASSWORD>',
+  process.env.DBPASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
